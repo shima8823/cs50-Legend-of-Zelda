08a21d051c3a27869abda8be8ed9d48ad41e6333
shima8823 <taiyaki1275@gmail.com>
2023-05-01 21:19:51 +0900

    pot lift animationを追加

--- src/entity_defs.lua
+++ src/entity_defs.lua
@@ -69,7 +69,31 @@ ENTITY_DEFS = {
                 interval = 0.05,
                 looping = false,
                 texture = 'character-swing-sword'
-            }
+            },
+            ['pot-lift-left'] = {
+                frames = {10, 11, 12},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
+            ['pot-lift-right'] = {
+                frames = {4, 5, 6},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
+            ['pot-lift-down'] = {
+                frames = {1, 2, 3},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
+            ['pot-lift-up'] = {
+                frames = {7, 8, 9},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
         }
     },
     ['skeleton'] = {
