8853484fbc8753ef111d315dfdc223ab1e9f28f8
shima8823 <taiyaki1275@gmail.com>
2023-05-06 09:42:45 +0900

    throw animationを追加

--- src/entity_defs.lua
+++ src/entity_defs.lua
@@ -130,6 +130,30 @@ ENTITY_DEFS = {
                 looping = false,
                 texture = 'character-pot-lift'
             },
+            ['pot-throw-left'] = {
+                frames = {12, 10},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
+            ['pot-throw-right'] = {
+                frames = {6, 4},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
+            ['pot-throw-down'] = {
+                frames = {3, 1},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
+            ['pot-throw-up'] = {
+                frames = {9, 7},
+                interval = 0.15,
+                looping = false,
+                texture = 'character-pot-lift'
+            },
         }
     },
     ['skeleton'] = {
